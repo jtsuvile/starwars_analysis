f4e2fb3e0b1e1eae913dc97ec91e5086c246f93a
Juulia Suvilehto <juulia.suvilehto@liu.se>
2025-04-07 16:41:09 +0200

    more complete documentation in schema.yml

--- models/fct_film_appearances.sql
+++ models/fct_film_appearances.sql
@@ -1,27 +1,27 @@
 with people_appearances as (select 
-  url
+  url as film_url
   , 'person' as resource_type
-  , trim(regexp_split_to_table(trim(characters, '[]'), ','), ''' ') as resource
+  , trim(regexp_split_to_table(trim(characters, '[]'), ','), ''' ') as resource_url
   from {{ ref('raw_films') }}),
 
 species_appearances as (select 
-  url
+  url as film_url
   , 'species' as resource_type
-  , trim(regexp_split_to_table(trim(species, '[]'), ','), ''' ') as resource
+  , trim(regexp_split_to_table(trim(species, '[]'), ','), ''' ') as resource_url
   from {{ ref('raw_films') }}
   ),
 
 starship_appearances as (select 
-  url
+  url as film_url
   , 'starship' as resource_type
-  , trim(regexp_split_to_table(trim(starships, '[]'), ','), ''' ') as resource
+  , trim(regexp_split_to_table(trim(starships, '[]'), ','), ''' ') as resource_url
   from {{ ref('raw_films') }}
   ),
   
 vehicle_appearances as (select
-    url
+    url as film_url
   , 'vehicle' as resource_type
-  , trim(regexp_split_to_table(trim(vehicles, '[]'), ','), ''' ') as resource
+  , trim(regexp_split_to_table(trim(vehicles, '[]'), ','), ''' ') as resource_url
   from {{ ref('raw_films') }}
   ),
 
