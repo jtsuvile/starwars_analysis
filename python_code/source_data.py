0163dd890e76fbfd13b6d1913f51e0b39fc80617
Juulia Suvilehto <juulia.suvilehto@liu.se>
2025-04-07 15:57:51 +0200

    going back to seeds with the data

--- python_code/source_data.py
+++ python_code/source_data.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def make_request(query):
-    """Get response to a request""" 
+    """Get response to a request"""
     response = requests.get(query)
     # TODO: could have more robust error handling for API calls, e.g. retries
     response.raise_for_status()
@@ -18,7 +18,7 @@ def make_request(query):
 def fetch_resource(query):
     """Fetch a response and convert to pandas dataframe"""
 
-    # TODO: If this was a function I would use more broadly, 
+    # TODO: If this was a function I would use more broadly,
     # I would probably make flags to control
     # which variables get returned.
     resources = make_request(query)
@@ -40,8 +40,8 @@ def get_all_of(url, resource_type):
         df = pd.concat([df, df_page])
         counter += 1
 
-    logging.info(f"Fetched {len(df)} rows of {resource_type} using {counter} \
-                 calls to the SWapi")
+    logging.info(f"Fetched {len(df)} rows of {resource_type} using {counter} " +
+                 "calls to the SWapi")
 
     # Check if we got all resources we expected
     # I'm not 100% sure if this should be an error instead of a warning.
@@ -65,7 +65,7 @@ def main():
     url = config.get('general', 'url')
     relevant_tables = config.items("tables")
 
-    # read in each table as defined in the config file 
+    # read in each table as defined in the config file
     # and save them as seeds for dbt to process
     for key, table in relevant_tables:
         resource_type = key
@@ -76,11 +76,11 @@ def main():
         # TODO: more robust quality control for the different columns
         if resource_type == 'starships':
             df['length'] = df['length'].str.replace(',', '')
-        df.to_csv(pathlib.Path(__file__).parent.parent / 'raw_data_files' /
+        df.to_csv(pathlib.Path(__file__).parent.parent / 'seeds' /
                   f'raw_{resource_type}.csv')
 
-    logging.info(f'Done with importing the following tables:\
-                 {", ".join(str(i[0]) for i in relevant_tables)}')
+    logging.info('Done with importing the following tables:' +
+                 f'{", ".join(str(i[0]) for i in relevant_tables)}')
 
 
 if __name__ == "__main__":
